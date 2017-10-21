a7d4b6cf09978712e464ed5482660d17abb159a7
smilewood <milewood@gmail.com>
2017-10-21 12:56:34 -0600

    stuff

---
+++ Assets/showMenu.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class showMenu : MonoBehaviour {
+    public GameObject menu;
+	void OnSelect()
+    {
+        menu.SetActive(true);
+    }
+    public void hideMenu()
+    {
+        menu.SetActive(false);
+    }
+}
