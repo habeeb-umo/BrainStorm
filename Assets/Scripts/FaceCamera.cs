3bb62a09809464c2d2bfd3f65a57ebf7f0fd3218
vicweeks <victorjweeks@gmail.com>
2017-10-21 11:12:33 -0600

    text and bubble texture

---
+++ Assets/Scripts/FaceCamera.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaceCamera : MonoBehaviour {
+    GameObject cmer; 
+	// Use this for initialization
+	void Start () {
+        cmer = Camera.main.gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.LookAt(cmer.transform);
+	}
+}
