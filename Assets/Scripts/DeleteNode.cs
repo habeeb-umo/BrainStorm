e2b53f92342cfa422d89720ef86e1f85e1a6a882
smilewood <milewood@gmail.com>
2017-10-21 21:47:08 -0600

    pop bubbles

--- Assets/Scripts/DeleteNode.cs
+++ Assets/Scripts/DeleteNode.cs
@@ -3,9 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DeleteNode : MonoBehaviour {
-
+	public GameObject partgo;
 	public void DeleteThisNode()
 	{
-		Destroy(this.gameObject);
+		var exp = partgo.GetComponent<ParticleSystem>();
+		
+        exp.Play();
+		partgo.GetComponent<MeshRenderer>().enabled = false;
+		this.gameObject.transform.Find("Menu").gameObject.SetActive(false);
+		this.gameObject.transform.Find("Text").gameObject.SetActive(false);
+        Destroy(gameObject, exp.duration);
 	}
 }
