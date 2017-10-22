ca18aae0413dc853727e9e4724718437dbf8e9db
Joshua Lutz <MystikalPooka@gmail.com>
2017-10-22 13:16:12 -0600

    Merging Linerender

---
+++ Assets/LineConnector.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineConnector : MonoBehaviour {
+    public GameObject LinePrefab;
+	// Use this for initialization
+	
+    public void Connect(GameObject a, GameObject b)
+    {
+        var AtoB = (b.transform.position - a.transform.position);
+        var distAB = AtoB.magnitude;
+        Vector3 posA = a.transform.position + ((AtoB / distAB) * (a.transform.Find("Text").GetComponent<RectTransform>().rect.width / 2));
+
+        var BtoA = (a.transform.position - b.transform.position);
+        var distBA = BtoA.magnitude;
+        Vector3 posB = a.transform.position + ((BtoA / distBA) * (b.transform.Find("Text").GetComponent<RectTransform>().rect.width / 2));
+        GameObject go = Instantiate(LinePrefab, this.transform);
+        go.GetComponent<DrawLine>().origin = posA;
+        go.GetComponent<DrawLine>().destination = posB;
+
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ Assets/LineRender/DrawLine.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//[ExecuteInEditMode]
+public class DrawLine : MonoBehaviour {
+
+	private LineRenderer lineRenderer;
+	private float counter;
+	private float dist;
+
+	public Vector3 origin;
+	public Vector3 destination;
+
+	public float lineDrawSpeed = 6f;
+
+	void Start () {
+
+		
+		
+	}
+	
+
+	void Update () {
+        if (origin != null && destination != null )
+        {
+            lineRenderer = GetComponent<LineRenderer>();
+            lineRenderer.SetPosition(0, origin);
+
+            dist = Vector3.Distance(origin, destination);
+        }
+		if (counter < dist) {
+			counter += .1f / lineDrawSpeed;
+			float x = Mathf.Lerp (0, dist, counter);
+
+			Vector3 pointA = origin;
+			Vector3 pointB = destination;
+
+			Vector3 pointAlongLine = x * Vector3.Normalize (pointB - pointA) + pointA;
+
+			lineRenderer.SetPosition (1, pointAlongLine);
+
+		}
+		
+	}
+}
