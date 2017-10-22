f92aced2cb4bec9c7449726a3f2ca5533481653d
smilewood <milewood@gmail.com>
2017-10-21 18:59:29 -0600

    UI Stuffs

---
+++ Assets/highlightButton.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class highlightButton : MonoBehaviour {
+
+	public Sprite sctiveSprites[2];
+	bool Active = false;
+
+	void OnLookedAt()
+	{
+		
+	}
+}
