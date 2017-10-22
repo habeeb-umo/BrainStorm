d3db7cb885c31385e47a4861bb83ae60c63f0cd5
Joshua Lutz <MystikalPooka@gmail.com>
2017-10-21 20:54:44 -0600

    Voice command... maybe?

--- Assets/Scripts/Bubble.cs
+++ Assets/Scripts/Bubble.cs
@@ -1,14 +1,14 @@
 ﻿using UnityEngine;
+using HoloToolkit.Unity.InputModule;
 
-public class Node : MonoBehaviour
+public class  Bubble : MonoBehaviour
 {
     public string Name;
     public string Contents;
+
 	// Use this for initialization
 	void Start ()
-    {
-		
-	}
+    {}
 	
 	// Update is called once per frame
 	void Update ()
@@ -19,12 +19,16 @@ public class Node : MonoBehaviour
     // Called by GazeGestureManager when the user performs a Select gesture
     void OnSelect()
     {
-        
         Debug.Log("MEOW! (also you clicked on things)");
     }
 
     void Remove()
     {
         Debug.Log("And now we are removing things!! MEOW MUTHATRUCKAS");
+    }
+
+    public void InstantiateNewBubble()
+    {
+
     }
 }
